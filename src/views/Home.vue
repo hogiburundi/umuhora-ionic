0a67be172f2f293168712dad1dd1af0cea1fd939
INGANZAMARUMPU <jonkur96@gmail.com>
2022-03-10 15:49:32 +0200

    made ion-tab-bar visible

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,23 +10,9 @@
     </ion-header>
     <ion-content class="ion-padding">
         <ion-router-outlet/>
-          <ion-tab-bar slot="bottom">
-            <ion-tab-button tab="vente" href="/home/vente">
-              <ion-icon :src="getIcon('calendar')"></ion-icon>
-              <ion-label>Vente</ion-label>
-            </ion-tab-button>
-            <ion-tab-button tab="stock" href="/home/stock">
-              <ion-icon :src="getIcon('personCircle')"></ion-icon>
-              <ion-label>Stock</ion-label>
-            </ion-tab-button>
-            <ion-tab-button tab="stats" href="/home/stats">
-              <ion-icon :src="getIcon('personCircle')"></ion-icon>
-              <ion-label>Statistiques</ion-label>
-            </ion-tab-button>
-          </ion-tab-bar>
-          <ion-fab-button class="todo-fab" @click="addTodo">
-            <ion-icon :src="getIcon('qrCode')"></ion-icon>
-          </ion-fab-button>
+      <ion-fab-button class="todo-fab" @click="">
+        <ion-icon :src="getIcon('qrCode')"></ion-icon>
+      </ion-fab-button>
     </ion-content>
   </ion-page>
 </template>
@@ -36,7 +22,7 @@
 <style>
   ion-fab-button{
     position: fixed;
-    right: 20px;
-    bottom: 50px;
+    right: 10px;
+    bottom: 70px;
   }
 </style>
