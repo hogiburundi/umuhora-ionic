f27a657cbfd595c41db97907a8efe236f9f9febe
INGANZAMARUMPU <jonkur96@gmail.com>
2022-07-20 15:38:30 +0200

    added client form fields on cart

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -17,7 +17,15 @@
         <b>{{ money(cart.getTotal()) }} </b>
         <div> BIF</div>
       </div>
-      <ion-col class="create">
+      <center @click="show_client_form = !show_client_form">
+        <a href="#" v-if="!show_client_form">
+          Afficher le formulaire client
+        </a>
+        <a href="#" v-else>
+          Fermer le formulaire client
+        </a>
+      </center>
+      <ion-col class="create" v-if="show_client_form">
         <ion-item class="ion-no-padding">
           <ion-label position="floating">Nom du client</ion-label>
           <ion-input type=text placeholder="Nom du client"
@@ -27,6 +35,144 @@
           <ion-label position="floating">Télephone du client</ion-label>
           <ion-input type="number" placeholder="Télephone du client"
             :value="tel" @IonChange="setTel($event.target.value)" clearInput/>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Invoice Type</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="invoice_type=$event.target.value"
+            :value="invoice_type"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="FN">
+              Facture normale
+            </ion-select-option>
+            <ion-select-option value="RC">
+              Remboursement caution
+            </ion-select-option>
+            <ion-select-option value="RHF">
+              Réduction Hors Facture
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">VAT Taxpayer</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="vat_taxpayer=$event.target.value"
+            :value="vat_taxpayer"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="0">
+              Non assujetti à la TVA
+            </ion-select-option>
+            <ion-select-option value="1">
+              Assujetti à la TVA
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">CT Taxpayer</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="ct_taxpayer=$event.target.value"
+            :value="ct_taxpayer"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="0">
+              Non Assujetti à la taxe de consommation
+            </ion-select-option>
+            <ion-select-option value="1">
+              Assujetti à la taxe de consommation
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">TL Taxpayer</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="tl_taxpayer=$event.target.value"
+            :value="tl_taxpayer"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="0">
+              Non Assujetti au prélèvement forfaitaire libératoire
+            </ion-select-option>
+            <ion-select-option value="1">
+              Assujetti au prélèvement forfaitaire libératoire
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Payment Type</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="payment_type=$event.target.value"
+            :value="payment_type"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="1">
+              en espèce
+            </ion-select-option>
+            <ion-select-option value="2">
+              banque
+            </ion-select-option>
+            <ion-select-option value="3">
+              à crédit
+            </ion-select-option>
+            <ion-select-option value="4">
+              autres
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Invoice Currency</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="invoice_currency=$event.target.value"
+            :value="invoice_currency"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="BIF">
+              Franc Burundais
+            </ion-select-option>
+            <ion-select-option value="USD">
+              United State Dollar
+            </ion-select-option>
+            <ion-select-option value="EUR">
+              EURO
+            </ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Customer Name</ion-label>
+          <ion-input type="text" 
+            placeholder="Customer Name"
+            @IonChange="customer_name=$event.target.value"
+            :value="customer_name" clearInput/>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Customer TIN</ion-label>
+          <ion-input type="text" 
+            placeholder="Customer TIN"
+            @IonChange="customer_TIN=$event.target.value"
+            :value="customer_TIN" clearInput/>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">Customer Address</ion-label>
+          <ion-input type="text" 
+            placeholder="Customer Address"
+            @IonChange="customer_address=$event.target.value"
+            :value="customer_address" clearInput/>
+        </ion-item>
+        <ion-item class="ion-no-padding">
+          <ion-label position="floating">VAT Customer Payer</ion-label>
+          <ion-select
+            multiple="false"
+            @IonChange="vat_customer_payer=$event.target.value"
+            :value="vat_customer_payer"
+            cancel-text="laisser" ok-text="valider">
+            <ion-select-option value="0">
+              Non assujetti à la TVA
+            </ion-select-option>
+            <ion-select-option value="1">
+              assujetti à la TVA
+            </ion-select-option>
+          </ion-select>
         </ion-item>
       </ion-col>
       <div class="clients" v-if="nom.length>0 || tel.length>0">
@@ -62,8 +208,28 @@ export default {
   components:{ CartItem },
   data(){
     return {
-      nom:"", tel:"", paid:this.$store.state.cart.getTotal(), clients:[],
-      active_client:null, found_clients:[]
+      nom:"",
+      tel:"",
+      paid:this.$store.state.cart.getTotal(),
+      clients:[],
+      active_client:null,
+      found_clients:[],
+
+      show_client_form:false,
+
+      invoice_type: "",
+      vat_taxpayer: "",
+      ct_taxpayer: "",
+      tl_taxpayer: "",
+      vat_taxpayer: "",
+      ct_taxpayer: "",
+      tl_taxpayer: "",
+      payment_type: "",
+      invoice_currency: "",
+      customer_name: "",
+      customer_TIN: "",
+      customer_address: "",
+      vat_customer_payer: ""
     }
   },
   computed:{
